1776ee0cad66127e74b7828c04d9b17c7f621300
crsantos16 <christian.santos.94@my.csun.edu>
2016-11-06 23:39:14 -0800

    Add files via upload

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,21 +1,23 @@
 # Add your VPC ID to default below
 variable "vpc_id" {
   description = "VPC ID for usage throughout the build process"
-  default = ""
+  default = "vpc-8955b0ee"
 }
 
 provider "aws" {
   region = "us-west-2"
 }
 
+# Internet gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${var.vpc_id}"
 
   tags = {
-    Name = "default_ig"
+    Name = "Internet Gateway"
   }
 }
 
+# Public Route Table 
 resource "aws_route_table" "public_routing_table" {
   vpc_id = "${var.vpc_id}"
   route {
@@ -28,9 +30,39 @@ resource "aws_route_table" "public_routing_table" {
   }
 }
 
+# Private Route Table 
+resource "aws_route_table" "private_routing_table" {
+  vpc_id = "${var.vpc_id}"
+
+  tags {
+    Name = "private_routing_table"
+  }
+}
+
+resource "aws_route" "private_route" {
+  route_table_id  = "${aws_route_table.private_routing_table.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = "${aws_nat_gateway.nat.id}"
+}
+
+# Nat Gateway 
+resource "aws_nat_gateway" "nat" {
+    allocation_id = "${aws_eip.tuto_eip.id}"
+    subnet_id = "${aws_subnet.private_subnet_a.id}"
+    depends_on = ["aws_internet_gateway.gw"]
+  
+}
+
+# Elastic IP
+resource "aws_eip" "tuto_eip" {
+  vpc      = true
+  depends_on = ["aws_internet_gateway.gw"]
+}
+
+# 3 Public subnet
 resource "aws_subnet" "public_subnet_a" {
     vpc_id = "${var.vpc_id}"
-    cidr_block = ""
+    cidr_block = "172.31.0.0/24"
     availability_zone = "us-west-2a"
 
     tags {
@@ -38,7 +70,96 @@ resource "aws_subnet" "public_subnet_a" {
     }
 }
 
+resource "aws_subnet" "public_subnet_b" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "172.31.2.0/24"
+    availability_zone = "us-west-2b"
+
+    tags {
+        Name = "public_b"
+    }
+}
+
+resource "aws_subnet" "public_subnet_c" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "172.31.3.0/24"
+    availability_zone = "us-west-2c"
+
+    tags {
+        Name = "public_c"
+    }
+}
+
+# 3 Private subnet
+resource "aws_subnet" "private_subnet_a" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "172.31.4.0/22"
+    availability_zone = "us-west-2a"
+
+    tags {
+        Name = "private_a"
+  }
+}
+
+resource "aws_subnet" "private_subnet_b" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "172.31.8.0/22"
+    availability_zone = "us-west-2b"
+
+    tags {
+        Name = "private_b"
+  }
+}
+
+resource "aws_subnet" "private_subnet_c" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "172.31.12.0/22"
+    availability_zone = "us-west-2c"
+
+    tags {
+        Name = "private_c"
+  }
+}
+# Route Table Associations for private subnet
+resource "aws_route_table_association" "private_subnet_a_rt_association" {
+    subnet_id = "${aws_subnet.private_subnet_a.id}"
+    route_table_id = "${aws_route_table.private_routing_table.id}"
+}
+
+resource "aws_route_table_association" "private_subnet_b_rt_association" {
+    subnet_id = "${aws_subnet.private_subnet_b.id}"
+    route_table_id = "${aws_route_table.private_routing_table.id}"
+}
+
+resource "aws_route_table_association" "private_subnet_c_rt_association" {
+    subnet_id = "${aws_subnet.private_subnet_c.id}"
+    route_table_id = "${aws_route_table.private_routing_table.id}"
+}
+
+# Route Table Associations for public subnet
 resource "aws_route_table_association" "public_subnet_a_rt_assoc" {
     subnet_id = "${aws_subnet.public_subnet_a.id}"
     route_table_id = "${aws_route_table.public_routing_table.id}"
 }
+
+resource "aws_route_table_association" "public_subnet_b_rt_assoc" {
+    subnet_id = "${aws_subnet.public_subnet_b.id}"
+    route_table_id = "${aws_route_table.public_routing_table.id}"
+}
+
+resource "aws_route_table_association" "public_subnet_c_rt_assoc" {
+    subnet_id = "${aws_subnet.public_subnet_c.id}"
+    route_table_id = "${aws_route_table.public_routing_table.id}"
+}
+
+# EC2 Instance 
+resource "aws_instance" "AMI_Instance" {
+    ami = "ami-b04e92d0"
+    associate_public_ip_address = true
+    subnet_id = "${aws_subnet.public_subnet_a.id}"
+    instance_type = "t2.micro"
+    tags {
+        Name = "AMI Instance"
+    }
+}
+
